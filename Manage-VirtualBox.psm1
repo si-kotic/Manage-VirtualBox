9e89cedf08dbcacb2998cb65453eae4b994aae2d
Simon Brown <simon@thehjclan.co.uk>
2021-05-07 15:21:57 +0100

    Tried to improve code to use an object but currently commented out

--- Manage-VirtualBox.psm1
+++ Manage-VirtualBox.psm1
@@ -1,14 +1,26 @@
 Function Get-VMs {
 	$Report = "" | Select-Object -Property Name,OS,Memory,CPUs,PowerState
+    $VMsObj = @()
 	. 'C:\Program Files\Oracle\VirtualBox\VBoxManage.exe' list vms | ForEach-Object {
-		$Report.Name = $_.Split('"')[1]
-		$fullInfo = . 'C:\Program Files\Oracle\VirtualBox\VBoxManage.exe' showvminfo $Report.Name
+        # $vmName = $_.Split('"')[1]
+		# $fullInfo = . 'C:\Program Files\Oracle\VirtualBox\VBoxManage.exe' showvminfo $vmName
+        # $vmProperties = @{
+        #     "Name" = $vmName,
+        #     "OS" = ($fullInfo | Where-Object {$_ -like "Guest OS*"}).SubString(17)
+        #     "Memory" = ($fullInfo | Where-Object {$_ -like "Memory Size*"}).SubString(17)
+        #     "CPUs" = ($fullInfo | Where-Object {$_ -like "Number of CPUs*"}).SubString(17)
+        #     "PowerState" = ($fullInfo | Where-Object {$_ -like "State*"}).SubString(17)
+        # }
+		# $Report += New-Object -TypeName PSObject -Property $vmProperties
+        $Report.Name = $_.Split('"')[1]
+        $fullInfo = . 'C:\Program Files\Oracle\VirtualBox\VBoxManage.exe' showvminfo $Report.Name
 		$Report.OS = ($fullInfo | Where-Object {$_ -like "Guest OS*"}).SubString(17)
 		$Report.Memory = ($fullInfo | Where-Object {$_ -like "Memory Size*"}).SubString(17)
 		$Report.CPUs = ($fullInfo | Where-Object {$_ -like "Number of CPUs*"}).SubString(17)
 		$Report.PowerState = ($fullInfo | Where-Object {$_ -like "State*"}).SubString(17)
-		$Report
+        $Report
 	}
+    # $Report
 }
 function Start-VM {
     Params (
